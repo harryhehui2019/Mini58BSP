e3f1c2abcd850e554d222e1aa2bcca952bbefbe4
ccwang <ccwang14@nuvoton.com>
2016-07-26 11:10:47 +0800

    Added register based SPI FIFO mode sample code.
    Change-Id: Ia842232d560830e418b45d0c1f0b82a5427ff437

---
+++ SampleCode/RegBased/SPI_SlaveFIFOMode/main.c
@@ -0,0 +1,169 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V1.00
+ * $Revision: 1 $
+ * $Date: 15/11/05 11:33a $
+ * @brief    Mini58 SPI Driver Sample Code
+ *           This is a SPI slave mode demo and need to be tested with a master device.
+ *
+ * @note
+ * Copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include "Mini58Series.h"
+
+#define TEST_COUNT 16
+
+uint32_t g_au32SourceData[TEST_COUNT];
+uint32_t g_au32DestinationData[TEST_COUNT];
+volatile uint32_t g_u32TxDataCount;
+volatile uint32_t g_u32RxDataCount;
+volatile uint8_t g_u8Done;
+
+void SYS_Init(void)
+{
+     int32_t i32TimeOutCnt;
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Unlock protected registers */
+    while(SYS->REGLCTL != 1) {
+        SYS->REGLCTL = 0x59;
+        SYS->REGLCTL = 0x16;
+        SYS->REGLCTL = 0x88;
+    }
+
+    /* Read User Config to select internal high speed RC */
+    SystemInit();
+
+    /* Enable external 12MHz XTAL, HIRC */
+    CLK->PWRCTL |= CLK_PWRCTL_XTL12M | CLK_PWRCTL_HIRCEN_Msk;
+
+    /* Waiting for clock ready */
+    i32TimeOutCnt = __HSI / 200; /* About 5ms */
+    while((CLK->STATUS & (CLK_STATUS_XTLSTB_Msk | CLK_STATUS_HIRCSTB_Msk)) !=
+            (CLK_STATUS_XTLSTB_Msk | CLK_STATUS_HIRCSTB_Msk)) {
+        if(i32TimeOutCnt-- <= 0)
+            break;
+    }
+
+    /* Switch HCLK clock source to XTL, STCLK to XTL */
+    CLK->CLKSEL0 = CLK_CLKSEL0_STCLKSEL_XTAL | CLK_CLKSEL0_HCLKSEL_XTAL;
+
+    /* Enable IP clock */
+    CLK->APBCLK = CLK_APBCLK_UART0CKEN_Msk;
+
+    /* Select IP clock source */
+    CLK->CLKSEL1 = CLK_CLKSEL1_UARTSEL_XTAL;
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set P1 multi-function pins for UART RXD and TXD */
+    SYS->P1_MFP &= ~(SYS_MFP_P12_Msk | SYS_MFP_P13_Msk);
+    SYS->P1_MFP |= (SYS_MFP_P12_UART0_RXD | SYS_MFP_P13_UART0_TXD);
+
+    /* Setup SPI multi-function pin */
+    SYS->P0_MFP |= SYS_MFP_P04_SPI0_SS | SYS_MFP_P05_SPI0_MOSI | SYS_MFP_P06_SPI0_MISO | SYS_MFP_P07_SPI0_CLK;
+
+    /* Lock protected registers */
+    SYS->REGLCTL = 0;
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+}
+
+void UART_Init(void)
+{
+   /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset IP */
+    SYS->IPRST1 |=  SYS_IPRST1_UART0RST_Msk;
+    SYS->IPRST1 &= ~SYS_IPRST1_UART0RST_Msk;
+
+    /* Configure UART and set UART Baudrate */
+    UART0->BAUD = UART_BAUD_BAUDM1_Msk | UART_BAUD_BAUDM0_Msk | (((__XTAL + (115200/2)) / 115200)-2);
+    UART0->LINE = 0x3 | (0x0 << UART_LINE_PBE_Pos) | (0x0 << UART_LINE_NSB_Pos) ;
+}
+
+void SPI_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init SPI                                                                                                */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Configure as a master, clock idle low, falling clock edge Tx, rising edge Rx and 32-bit transaction */
+    /* Set IP clock divider. SPI clock rate = 2MHz */
+    CLK->APBCLK |= CLK_APBCLK_SPICKEN_Msk;
+    SPI->CTL = SPI_SLAVE | SPI_MODE_0;
+    SPI->CLKDIV = (((12000000 / 2000000) + 1) >> 1) - 1;
+
+    /* Enable the automatic hardware slave select function. Select the SS pin and configure as low-active. */
+    SPI->SSCTL |= (SPI_SS | SPI_SS_ACTIVE_LOW) | SPI_SSCTL_SSLTEN_Msk;
+}
+
+void SPI_IRQHandler(void)
+{
+    uint32_t temp;
+
+     while( !(SPI->STATUS & SPI_STATUS_TXFULL_Msk) && (g_u32TxDataCount<TEST_COUNT) ) {
+        SPI->TX = g_au32SourceData[g_u32TxDataCount++];
+    }
+	
+    while(!(SPI->STATUS & SPI_STATUS_RXEMPTY_Msk)) {
+        temp = SPI->RX;
+        g_au32DestinationData[g_u32RxDataCount++] = temp;
+    }
+	
+    if(g_u32TxDataCount>=TEST_COUNT) {
+        SPI->FIFOCTL &= ~SPI_FIFOCTL_TXTHIEN_Msk; /* Disable TX FIFO threshold interrupt */         
+        g_u8Done = 1;
+    }
+}
+
+int main(void)
+{
+    uint32_t u32DataCount;
+
+    /* Init System, IP clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART for printf */
+    UART_Init();
+
+    /* Init SPI */
+    SPI_Init();
+    
+    printf("\n\n");
+    printf("+----------------------------------------------------------------------+\n");
+    printf("|                       SPI Driver Sample Code                         |\n");
+    printf("+----------------------------------------------------------------------+\n");
+    printf("\n");
+
+    printf("Configure SPI as a slave.\n");
+    
+    for(u32DataCount=0; u32DataCount<TEST_COUNT; u32DataCount++) {
+        g_au32SourceData[u32DataCount] = 0x00550000 + u32DataCount;
+        g_au32DestinationData[u32DataCount] = 0;
+    }    
+   
+    SPI->CTL |= SPI_CTL_FIFOEN_Msk;
+    SPI->FIFOCTL |= (SPI_FIFOCTL_RXTHIEN_Msk | SPI_FIFOCTL_TXTHIEN_Msk);
+    SPI->FIFOCTL = (SPI->FIFOCTL & ~(SPI_FIFOCTL_TXTH_Msk | SPI_FIFOCTL_RXTH_Msk) |
+                     (2 << SPI_FIFOCTL_TXTH_Pos) |
+                     (1 << SPI_FIFOCTL_RXTH_Pos));
+    NVIC_EnableIRQ(SPI_IRQn);
+	
+    while(!g_u8Done);
+    
+    printf("Received data:\n");
+    for(u32DataCount=0; u32DataCount<TEST_COUNT; u32DataCount++) {
+        printf("%d:\t0x%08X\n", u32DataCount, g_au32DestinationData[u32DataCount]);
+    }
+        
+    printf("The data transfer was done.\n");
+
+    while(1);
+}
